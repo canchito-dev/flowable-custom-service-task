808fc39f919f3e9e0fd9e9615386a1312c307abf
José Carlos Mendoza Prego <contact@canchito-dev.com>
2020-05-12 19:51:41 +0200

    Added unit testing

--- src/test/java/com/canchitodev/flowablecustomservicetasks/FlowableCustomServiceTasksApplicationTests.java
+++ src/test/java/com/canchitodev/flowablecustomservicetasks/FlowableCustomServiceTasksApplicationTests.java
@@ -1,13 +1,53 @@
 package com.canchitodev.flowablecustomservicetasks;
 
+import org.flowable.engine.HistoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.engine.test.Deployment;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 class FlowableCustomServiceTasksApplicationTests {
 
+	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
+	private HistoryService historyService;
+
 	@Test
-	void contextLoads() {
+	@Deployment(resources = "processes/my-first-custom-service-task.bpmn20.bpmn")
+	void myFirstCustomServiceTaskTest() {
+		// Start a new process instance
+		ProcessInstance processInstance = this.runtimeService.startProcessInstanceByKey("myFirstCustomServiceTask");
+
+		// Make sure the process has ended
+		await().atMost(30L, TimeUnit.SECONDS).until(
+				() -> this.historyService.createHistoricProcessInstanceQuery()
+						.processInstanceId(processInstance.getProcessInstanceId())
+						.finished()
+						.singleResult() != null
+		);
 	}
 
+	@Test
+	@Deployment(resources = "processes/my-first-field-injection.bpmn20.bpmn")
+	void myFirstFieldInjectionTest() {
+		// Start a new process instance
+		ProcessInstance processInstance = this.runtimeService.startProcessInstanceByKey("myFirstFieldInjection");
+
+		// Make sure the process has ended
+		await().atMost(30L, TimeUnit.SECONDS).until(
+				() -> this.historyService.createHistoricProcessInstanceQuery()
+						.processInstanceId(processInstance.getProcessInstanceId())
+						.finished()
+						.singleResult() != null
+		);
+	}
 }
